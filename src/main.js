0388e070ed3594ccbc957e6cfc41a275aa1db85f
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2024-09-30 16:53:41 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { fetchImages } from './js/pixabay-api.js';
-import { renderImages, showLoader, hideLoader } from './js/render-functions.js';
+import { renderImages } from './js/render-functions.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
